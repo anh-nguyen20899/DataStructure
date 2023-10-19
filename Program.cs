c63f615d8851c90402e311f962989e8ac8e183eb
anhng <anananh1999@gmail.com>
2023-10-19 17:59:37 +1100

    Merge Two Sorted Array

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace CTDL;
+using System.Collections;
+class Program
+{
+    public const int MaxLength = 12;
+    static void Main(string[] args)
+    {
+        Stack my_stack = new Stack();
+ 
+        // Adding elements in the Stack
+        // Using Push method
+        my_stack.Push("Geeks");
+        my_stack.Push("geeksforgeeks");
+        my_stack.Push("geeks23");
+        my_stack.Push("GeeksforGeeks");
+         if (my_stack.Contains("GeeksforGeeks") == true) 
+        {
+            Console.WriteLine("Element is found...!!");
+        }
+ 
+        else
+        {
+            Console.WriteLine("Element is not found...!!");
+        }
+    }
+}
